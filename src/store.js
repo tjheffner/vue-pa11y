2ec85ee11f2b75c9e821e739dba6181f03f91ad5
tjheffner <tannerjheffner@gmail.com>
2018-10-25 12:56:05 -0700

    move report location to tests/pa11y

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import _ from 'lodash';
-import * as data from '../report.json';
+import * as data from '../tests/pa11y/report.json';
 
 Vue.use(Vuex);
 
