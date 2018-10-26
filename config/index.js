63f3618a6616c7efa8c8f690f1e09abba72c6929
tjheffner <tannerjheffner@gmail.com>
2018-10-26 13:18:36 -0700

    fix host issue for docker support

--- config/index.js
+++ config/index.js
@@ -13,8 +13,9 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    disableHostCheck: true, // turn to false if not using 0.0.0.0 as a host
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
